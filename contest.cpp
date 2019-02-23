b7ab493f8920c3f4ffa34f1b260083aef8fae715
sfc9982 <sfc9982@outlook.com>
2019-02-23 21:02:13 +0800

    2333

--- contest.cpp
+++ contest.cpp
@@ -1,47 +1,81 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 #include <algorithm>
 
 using namespace std;
-int a[100050];
 
-int main()
+bool stat;
+int n, m, l, r, s;
+int x, y, p[100050], a[100050];
+vector<int> G[100050];
+
+void Search(int c)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    stable_sort(a, a + n);
-    cin >> m;
-    while (m--)
+    cin >> l >> r >> s;
+    int x = 0, y = 0;
+    stat = true;
+    for (int i = 2; i * i <= s; i++)
     {
-        int x, l = 0, r = n - 1, mid;
-        cin >> x;
-        if (x <= a[0])
+        y = 0;
+        if (s % i == 0)
         {
-            cout << a[0] << endl;
-            continue;
-        }
-        if (x >= a[n - 1])
-        {
-            cout << a[n - 1] << endl;
-            continue;
-        }
-        while (l + 1 < r)
-        {
-            mid = (l + r) / 2;
-            if (a[mid] > x)
-                r = mid;
-            else if (a[mid] < x)
-                l = mid;
-            else
+            while (s % i == 0)
             {
-                l = r = mid;
+                s /= i;
+                y++;
+            }
+            int t;
+            t = upper_bound(G[i].begin(), G[i].end(), r) - lower_bound(G[i].begin(), G[i].end(), l);
+            if (t < y)
+            {
+                stat = false;
                 break;
             }
         }
-        cout << (abs(a[l] - x) <= abs(a[r] - x) ? a[l] : a[r]) << endl;
+    }
+    if (s > 1)
+    {
+        x = s;
+        y = 1;
+        int t;
+        t = upper_bound(G[s].begin(), G[s].end(), r) - lower_bound(G[s].begin(), G[s].end(), l);
+        if (t < 1)
+            stat = 0;
+    }
+    if (stat)
+        cout << "Yes";
+    else
+        cout << "No";
+    if (c)
+        cout << endl;
+    return;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int a[100050];
+        cin >> n >> m;
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+            for (int j = 2; j * j <= a[i]; j++)
+            {
+                while (a[i] % j == 0)
+                {
+                    G[j].push_back(i);
+                    a[i] /= j;
+                }
+            }
+            if (a[i] > 1)
+                G[a[i]].push_back(i);
+        }
+        while (m--)
+            Search(m);
     }
     return 0;
 }
